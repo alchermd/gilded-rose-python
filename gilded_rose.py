c1fe91ea23b58a6f5ad3c8de6aa57bf87bc99875
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2023-01-14 21:18:55 +0800

    Refactor quality deduction logic with better variable names and extracted method for readability

--- gilded_rose.py
+++ gilded_rose.py
@@ -4,6 +4,8 @@ NORMAL_ITEM_NAME = "NORMAL-ITEM"
 BACKSTAGE_PASS_ITEM_NAME = "Backstage passes to a TAFKAL80ETC concert"
 AGED_BRIE_ITEM_NAME = "Aged Brie"
 LEGENDARY_ITEM_NAME = "Sulfuras, Hand of Ragnaros"
+PASS_ITEMS = [BACKSTAGE_PASS_ITEM_NAME, AGED_BRIE_ITEM_NAME]
+MAX_ITEM_QUALITY = 50
 
 
 class GildedRose(object):
@@ -11,22 +13,26 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
+    @staticmethod
+    def _quality_can_be_deducted(item: "Item") -> bool:
+        return item.quality > 0 and item.name != LEGENDARY_ITEM_NAME
+
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
+            if item.name in PASS_ITEMS:
+                if item.quality < MAX_ITEM_QUALITY:
+                    item.quality += 1
+                    if item.name == BACKSTAGE_PASS_ITEM_NAME:
                         if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality += 1
                         if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
+                            item.quality += 1
+
+                    item.quality = item.quality if item.quality < MAX_ITEM_QUALITY else MAX_ITEM_QUALITY
+            else:
+                if self._quality_can_be_deducted(item):
+                    item.quality = item.quality - 1
+
             if item.name != "Sulfuras, Hand of Ragnaros":
                 item.sell_in = item.sell_in - 1
             if item.sell_in < 0:
