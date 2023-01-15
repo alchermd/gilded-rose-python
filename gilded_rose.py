53f3bed12cf63774d11a7b470d64067d68f3ee06
John Alcher Doloiras <johnalcherdoloiras@gmail.com>
2023-01-15 11:38:12 +0800

    Refactor to shorten branches and move related logic to their own methods for readability

--- gilded_rose.py
+++ gilded_rose.py
@@ -8,44 +8,60 @@ PASS_ITEMS = [BACKSTAGE_PASS_ITEM_NAME, AGED_BRIE_ITEM_NAME]
 MAX_ITEM_QUALITY = 50
 
 
+def quality_can_be_deducted(item: "Item") -> bool:
+    return item.quality > 0 and item.name != LEGENDARY_ITEM_NAME
+
+
+def reduce_item_quality(item: "Item") -> "Item":
+    if item.name in PASS_ITEMS:
+        if item.quality < MAX_ITEM_QUALITY:
+            item.quality += 1
+
+            if item.name == BACKSTAGE_PASS_ITEM_NAME:
+                if item.sell_in < 11:
+                    item.quality += 1
+                if item.sell_in < 6:
+                    item.quality += 1
+
+            item.quality = item.quality if item.quality < MAX_ITEM_QUALITY else MAX_ITEM_QUALITY
+    elif quality_can_be_deducted(item):
+        item.quality = item.quality - 1
+
+    return item
+
+
+def reduce_item_sell_in(item: "Item") -> "Item":
+    if item.name != LEGENDARY_ITEM_NAME:
+        item.sell_in = item.sell_in - 1
+
+    return item
+
+
+def update_quality_for_expired_item(item: "Item") -> "Item":
+    if item.name == AGED_BRIE_ITEM_NAME:
+        if item.quality < MAX_ITEM_QUALITY:
+            item.quality = item.quality + 1
+    elif item.name != BACKSTAGE_PASS_ITEM_NAME:
+        if quality_can_be_deducted(item):
+            item.quality = item.quality - 1
+    else:
+        item.quality = 0
+
+    return item
+
+
 class GildedRose(object):
 
     def __init__(self, items):
         self.items = items
 
-    @staticmethod
-    def _quality_can_be_deducted(item: "Item") -> bool:
-        return item.quality > 0 and item.name != LEGENDARY_ITEM_NAME
-
     def update_quality(self):
         for item in self.items:
-            if item.name in PASS_ITEMS:
-                if item.quality < MAX_ITEM_QUALITY:
-                    item.quality += 1
-                    if item.name == BACKSTAGE_PASS_ITEM_NAME:
-                        if item.sell_in < 11:
-                            item.quality += 1
-                        if item.sell_in < 6:
-                            item.quality += 1
-
-                    item.quality = item.quality if item.quality < MAX_ITEM_QUALITY else MAX_ITEM_QUALITY
-            else:
-                if self._quality_can_be_deducted(item):
-                    item.quality = item.quality - 1
-
-            if item.name != LEGENDARY_ITEM_NAME:
-                item.sell_in = item.sell_in - 1
+            reduce_item_quality(item)
+            reduce_item_sell_in(item)
 
             if item.sell_in < 0:
-                if item.name == AGED_BRIE_ITEM_NAME:
-                    if item.quality < MAX_ITEM_QUALITY:
-                        item.quality = item.quality + 1
-                else:
-                    if item.name != BACKSTAGE_PASS_ITEM_NAME:
-                        if self._quality_can_be_deducted(item):
-                            item.quality = item.quality - 1
-                    else:
-                        item.quality = 0
+                update_quality_for_expired_item(item)
 
 
 class Item:
